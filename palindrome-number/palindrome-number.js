8f980f4fbaa5616c001b9896ee6ef45757e7386d
Radical Dilettante <66890884+waynethebb@users.noreply.github.com>
2021-06-28 21:54:59 +1200

    Update palindrome-number.js

--- palindrome-number/palindrome-number.js
+++ palindrome-number/palindrome-number.js
@@ -4,32 +4,15 @@
  */
 var isPalindrome = function(x) {
     let str = x.toString();
+    
     if(x < 0){
         return false;
     }
-    
-    let arr = str.split("");
-    
-    let mid = parseInt(arr.length / 2);
-   
-    let target1 = [];
-    let target2 = [];
-    
-    for (let i = 0; i < mid; i++){
-        target1.push(arr[i]);
-    }
-    
-    if(str.length % 2 === 1){
-        mid++;
-    }
-    
-    for (let i = mid; i < arr.length; i++){
-        target2.push(arr[i]);
-    }
-    
-    if(target1.join() === target2.reverse().join()){
+
+    if(str === str.split("").reverse().join("")){
         return true;
     } else{
         return false;
     }
+    
 };
