39f7f980d8040365cc659c53e33d0872c4038744
Wayne Choi <66890884+waynethebb@users.noreply.github.com>
2022-02-27 20:32:53 +1300

    Time: 68 ms (96.11%), Space: 43.8 MB (12.20%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -15,10 +15,10 @@
 // };
 
 
-const twoSum = function (nums, target) {
-    let map = new Map;
+var twoSum = function (nums, target) {
+    var map = new Map;
     for (var i = 0; i < nums.length; i++){
-        let complement = target - nums[i];
+        var complement = target - nums[i];
         if (map.has(complement)) {
             return [map.get(complement),i]
         };
